38401295bb67e938a9b859a9f6e62e703cecca72
natiorozco <nataliaorozcodelgado@gmail.com>
2024-10-08 17:00:50 -0600

    init

---
+++ src/main/java/org/adbbnod/main.java
@@ -0,0 +1,17 @@
+package org.adbbnod;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class main
+{
+    public static void main( String[] args )
+    {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
