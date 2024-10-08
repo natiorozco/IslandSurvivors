b7b357b3e8cd8d3713b2e39d8bf19b25e801a825
natiorozco <nataliaorozcodelgado@gmail.com>
2024-10-08 17:13:41 -0600

    Add main screen

--- src/main/java/org/adbbnod/main.java
+++ src/main/java/org/adbbnod/main.java
@@ -12,6 +12,16 @@ public class main
     {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Island Survivors");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
     }
 }
